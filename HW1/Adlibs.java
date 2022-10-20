fc16e3bae597f0730efe6fc5fb0cae24aee6dcca
michaeldmora <moram1@go.stockton.edu>
2022-10-20 18:06:54 -0400

    Finalized HW1

--- HW1/Adlibs.java
+++ HW1/Adlibs.java
@@ -1,9 +1,5 @@
 package HW1;
-// make a method that accepts 2 Strings and 1 int (Someones name, age, and favorite color and then returns a story about them)
-// Hint1: The objects name is Adlibs and you require a name age and favoriteColor all in strings
-// Hint2: You will need to return a string. You can concat the variables and the strings together
-// Hint3: A sample story if you are lost can be as simple as "Hello! My name is ____, and I am ____, and my favorite
-//        color is ___"
+
 public class Adlibs {
     public String story(String name, int age, String movie, int number) {
         return "Hello! My name is " + name + ", and I am " + age + " years old" + ", and my favorite movie is " + movie + ", and my favorite number is " + number + ".";
